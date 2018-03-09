215aacfe6c020e4b0414a7dfb70ae6e057fa201e
Malorzata <gosia.haniszewska@gmail.com>
2018-03-09 14:08:16 -0800

    send most of data to InitialState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,97 +11,6 @@ class App extends React.Component {
     super(props);
     console.log(props);
     this.state = {
-      masterRoomsList: {
-        1: {
-          name: 'Room1',
-          description: 'room description here',
-          numberOfGuests: 2,
-          seasonPrice: 100,
-          offSeasonPrice: 80,
-          reservations: {'1': {id: '1', firstDay: '2018-03-02', lastDay: '2018-03-12', firstName: 'John', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-05-02', lastDay: '2018-05-20', firstName: 'Matilda', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '1'
-        },
-        2: {
-          name: 'Room2',
-          description: 'room description here',
-          numberOfGuests: 2,
-          seasonPrice: 100,
-          offSeasonPrice: 80,
-          reservations: {'1': {id: '1', firstDay: '2018-08-02', lastDay: '2018-08-12', firstName: 'Sam', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-10-02', lastDay: '2018-10-20', firstName: 'Amanda', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '2'
-        },
-        3: {
-          name: 'Room3',
-          description: 'room description here',
-          numberOfGuests: 3,
-          seasonPrice: 130,
-          offSeasonPrice: 100,
-          reservations: {'1': {id: '1', firstDay: '2018-04-02', lastDay: '2018-04-12', firstName: 'Patric', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-06-02', lastDay: '2018-06-20', firstName: 'Emeshea', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '3'
-        },
-        4: {
-          name: 'Room4',
-          description: 'room description here',
-          numberOfGuests: 3,
-          seasonPrice: 130,
-          offSeasonPrice: 100,
-          reservations: {'1': {id: '1', firstDay: '2018-05-22', lastDay: '2018-05-30', firstName: 'Viola', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-05-02', lastDay: '2018-05-20', firstName: 'Bob', lastName: 'Doe', phone: '206 123 9876'} },
-        },
-        5: {
-          name: 'Room5',
-          description: 'room description here',
-          numberOfGuests: 3,
-          seasonPrice: 130,
-          offSeasonPrice: 100,
-          reservations: {'1': {id: '1', firstDay: '2018-04-22', lastDay: '2018-05-12', firstName: 'Margaret', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-02-02', lastDay: '2018-02-20', firstName: 'Linda', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '5'
-        },
-        6: {
-          name: 'Room6',
-          description: 'room description here',
-          numberOfGuests: 4,
-          seasonPrice: 150,
-          offSeasonPrice: 120,
-          reservations: {'1': {id: '1', firstDay: '2018-05-22', lastDay: '2018-05-30', firstName: 'Robbie', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-05-07', lastDay: '2018-05-27', firstName: 'Kevin', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '6'
-        },
-        7: {
-          name: 'Room7',
-          description: 'room description here',
-          numberOfGuests: 4,
-          seasonPrice: 150,
-          offSeasonPrice: 120,
-          reservations: {'1': {id: '1', firstDay: '2018-03-02', lastDay: '2018-03-20', firstName: 'Alvaro', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-01-02', lastDay: '2018-01-20', firstName: 'Chris', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '7'
-        },
-        8: {
-          name: 'Room8',
-          description: 'room description here',
-          numberOfGuests: 4,
-          seasonPrice: 150,
-          offSeasonPrice: 120,
-          reservations: {'1': {id: '1', firstDay: '2018-09-02', lastDay: '2018-09-12', firstName: 'Kim', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-08-22', lastDay: '2018-09-20', firstName: 'Rico', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '8'
-        },
-        9: {
-          name: 'Room9',
-          description: 'room description here',
-          numberOfGuests: 5,
-          seasonPrice: 200,
-          offSeasonPrice: 150,
-          reservations: {'1': {id: '1', firstDay: '2018-07-02', lastDay: '2018-07-12', firstName: 'Particia', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-05-02', lastDay: '2018-05-20', firstName: 'Josh', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '9'
-        },
-        10: {
-          name: 'Room10',
-          description: 'room description here',
-          numberOfGuests: 5,
-          seasonPrice: 200,
-          offSeasonPrice: 150,
-          reservations: {'1': {id: '1', firstDay: '2018-04-02', lastDay: '2018-04-30', firstName: 'Anna', lastName: 'Smith', phone: '206 123 4567'}, '2': {id: '2', firstDay: '2018-05-02', lastDay: '2018-06-04', firstName: 'Travis', lastName: 'Doe', phone: '206 123 9876'} },
-          id: '10'
-        }
-      },
       roomsFilter: {numberOfGuests: null, startDay: null, lastDay: null}
     };
     this.handleNewFilterCreation=this.handleNewFilterCreation.bind(this);
@@ -207,11 +116,11 @@ class App extends React.Component {
       <div className="container">
         <Switch>
           <Route exact path='/' render={()=><Guest
-            masterRoomsList={this.state.masterRoomsList}
+            masterRoomsList={this.props.masterRoomsList}
             onNewFilterCreation={this.handleNewFilterCreation}
             roomsFilter={this.state.roomsFilter} />} />
           <Route path='/admin' render={()=><Admin
-            masterRoomsList={this.state.masterRoomsList}
+            masterRoomsList={this.props.masterRoomsList}
             reservationsRequestsList={this.props.reservationsRequestsList}
             onSubmitReservationRequest={this.handleSubmitReservationRequest}
             onDeleteReservation={this.handleDeleteReservation}
@@ -224,7 +133,8 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    reservationsRequestsList: state.reservationsRequestsList
+    reservationsRequestsList: state.reservationsRequestsList,
+    masterRoomsList: state.masterRoomsList
   };
 };
 
